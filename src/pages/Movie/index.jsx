c4e1e5ae71e2b7c95eaf8d6da682104dbe1d5cf5
Wilmar dos Santos <dswilmar@gmail.com>
2022-04-15 11:13:04 -0300

    Renomeando funcao para carregar filmes

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import './style.css';
+
 function Movie() {
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        async function loadMovie() {
+            
+        }
+
+        loadMovie();
+    }, []);
+
     return (
         <div>
-            <h1>Movie</h1>
+            <h1>Movie {id}</h1>
         </div>
     )
 }
