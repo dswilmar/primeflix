45a803e7ca78275e78164c419747c72fc1854b7c
Wilmar dos Santos <dswilmar@gmail.com>
2022-04-15 10:55:38 -0300

    Listagem dos filmes e criado config .env

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import './style.css';
+
 function Home() {
+
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            const response = await api.get(`movie/now_playing`, {
+                params: {
+                    api_key: process.env.REACT_APP_API_KEY,
+                    language: 'pt-br',
+                    page: 1
+                }
+            });
+            //console.log(response.data.results.slice(0, 10));
+            setMovies(response.data.results.slice(0, 10));
+            setIsLoading(false);
+        }
+
+        loadFilmes();
+    }, []);
+
+    if (isLoading) {
+        return(
+            <div className="loading">
+                <h2>Carregando...</h2>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            <div className="movies-list">
+                { movies.map((movie) => {
+                    return (
+                        <article key={movie.id}>
+                            <h2>{movie.title}</h2>
+                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+                            <Link to={`/movie/${movie.id}`}>Ver detalhes</Link>
+                        </article>
+                    )
+                }) }
+            </div>
         </div>
     )
 }
