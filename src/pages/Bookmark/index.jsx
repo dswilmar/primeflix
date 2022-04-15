99cd203a2903034c4b7933e5104b16852695ddc2
Wilmar dos Santos <dswilmar@gmail.com>
2022-04-15 13:18:23 -0300

    Funcionalidade de excluir filmes salvos

--- src/pages/Bookmark/index.jsx
+++ src/pages/Bookmark/index.jsx
@@ -11,17 +11,28 @@ function Bookmark() {
         setMovies(JSON.parse(storageMovies) || []);
     }, []);
 
+    function removeMovie(movieId) {
+        let filter = movies.filter((item) => {
+            return (item.id !== movieId)
+        });
+
+        setMovies(filter);
+        localStorage.setItem('@primeflix', JSON.stringify(filter));
+    }
+
     return(
         <div className="bookmark">
             <h1>Meus Filmes</h1>
+            {movies.length === 0 && <p>Nenhum filme salvo ainda.</p>}
             <ul>
                 {movies.map((movie) => {
                     return(
                         <li key={movie.id}>
-                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
                             <h4>{movie.title}</h4>
                             <div>
                                 <Link to={`/filme/${movie.id}`}>Ver detalhes</Link>
+                                <button onClick={() => removeMovie(movie.id)}>Excluir</button>
                             </div>
                         </li>
                     )
